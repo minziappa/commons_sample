7594b8bd42cb5e6eea46c3dfe104f010faf88106
kim joon <minziappa@gmail.com>
2015-12-31 11:57:05 +0900

    Add Common Digester

--- src/main/java/io/commons/main/DigesterMain.java
+++ src/main/java/io/commons/main/DigesterMain.java
@@ -9,28 +9,26 @@ import io.commons.digester.UserInfos;
 
 public class DigesterMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 
 		Digester digester = new Digester();
 		digester.setValidating(false);
-		
+
 		digester.addObjectCreate("users", UserInfos.class);
 		digester.addObjectCreate("users/user", UserInfo.class);
 		digester.addBeanPropertySetter("users/user/id", "id");
 		digester.addBeanPropertySetter("users/user/name", "name");
 		digester.addBeanPropertySetter("users/user/age", "age");
 
-		File file = new File("test.xml");
+		File file = new File("./src/main/resources/userInfo.xml");
 
-		try {
-			UserInfos users = (UserInfos)digester.parse(file);
-			UserInfo user = users.getUserInfo("A01");
-			System.out.println(user.getName());
+		digester.addSetNext("users/user", "addUserInfo");
 
-		} catch (Exception e) {
-			// TODO: handle exception
-		} 
+		UserInfos users = (UserInfos)digester.parse(file);
+		
+		UserInfo user = users.getUserInfo("id1");
+		System.out.println(user.getName());
+
 	}
 
 }
